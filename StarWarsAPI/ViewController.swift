b701380e9bc3c5f566321fec5597bab79837175a
Giorgio Giannotta <jorge_upa@msn.com>
2021-01-07 19:49:51 +0100

    Initial Commit

---
+++ StarWarsAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWarsAPI
+//
+//  Created by Jorge Giannotta on 07/01/2021.
+//  Copyright © 2021 Westcostyle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
